ee92d1a8e323e59cd6dd84186e933e9b26f584cd
Евгения Голубенко <golubenko_e@MacBook-Pro-Evgenia.local>
2023-03-04 18:07:03 +0200

    fixed script

--- Medium/3_longest_substring_without_repeating_characters.py
+++ Medium/3_longest_substring_without_repeating_characters.py
@@ -1,37 +1,33 @@
-""" A peak element is an element that is strictly greater than its neighbors.
-
-Given a 0-indexed integer array nums, find a peak element, and return its index. 
-If the array contains multiple peaks, return the index to any of the peaks.
-
-You may imagine that nums[-1] = nums[n] = -∞. 
-In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.
-
-You must write an algorithm that runs in O(log n) time.
+""" Given a string s, find the length of the longest substring without repeating characters.
 
 ________________
 
 speed
-O(log(N))
+O(N)
 """
-from typing import List
+from collections import defaultdict
 
 
 class Solution:
 
-    def findPeakElement(self, nums: List[int]) -> int:
-        l, r = 0, len(nums) - 1
-        while l < r:
-            m = (l + r) // 2
-            if nums[m-1] < nums[m] > nums[m+1]:
-                return m
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        charset = defaultdict(int)
+        longest = left_pointer = 0
 
-            if nums[m] > nums[m+1]:
-                r = m
+        for right_pointer in range(len(s)):
+            char = s[right_pointer]
+
+            if charset.get(char, -1) < left_pointer:
+                longest = max(longest, (right_pointer-left_pointer+1))
             else:
-                l = m + 1
-        return l
+                left_pointer = charset[char] + 1
+
+            charset[char] = right_pointer
+        return longest
 
 
-print(Solution().findPeakElement([1, 2, 3, 1]))
-print(Solution().findPeakElement([1, 2, 1, 3, 5, 6, 4]))
-print(Solution().findPeakElement([3, 2, 1]))
+print(Solution().lengthOfLongestSubstring("abcabcbb"))
+print(Solution().lengthOfLongestSubstring("bbbbb"))
+print(Solution().lengthOfLongestSubstring("pwwkew"))
+print(Solution().lengthOfLongestSubstring(""))
+print(Solution().lengthOfLongestSubstring(" "))
